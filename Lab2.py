baa537fdd0a9d8e1c2db528e05b26e84e06cb4ee
Shraddha Dinesh Aher <shaher@syr.edu>
2025-09-04 14:30:40 +0000

    Lab1

---
+++ Lab2.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def app():
+    st.title("🔬 Lab 2 - Multi-Page Lab App")
+    st.write("This is the Lab 2 page. ✅")
+    st.write("You can paste your Lab 2 code here later.")
